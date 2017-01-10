cbbdf0c564535c29ff68a27f53c83f14bd627b0c
Shi Han NG <shihanng@gmail.com>
2017-01-11 08:20:44 +0900

    remove configured stuff from setup

--- setup.sh
+++ setup.sh
@@ -4,13 +4,8 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 pushd "$script_dir"
 base_dir=$(git rev-parse --show-toplevel)
 
-ln -sf "${base_dir}/gitconfig" $HOME/.gitconfig
-ln -sf "${base_dir}/tmux/tmux.conf" $HOME/.tmux.conf
 ln -sf "${base_dir}/vim/vimrc" $HOME/.vimrc
 ln -sf "${base_dir}/zsh/zshrc" $HOME/.zshrc
 ln -sf "${base_dir}/nvim/init.vim" $HOME/.config/nvim/init.vim
 ln -sf "${base_dir}/i3" $HOME/.i3
-ln -sf "${base_dir}/peco" $HOME/.config/peco
-
-cp -f "${base_dir}/gitconfig.local" $HOME/.gitconfig.local
 

add7c2c5abc6ee2451e8cdfc070c5755a556cbfc
Shi Han NG <shihanng@gmail.com>
2018-08-26 20:47:47 +0900

    zshrc: add fzf

---
+++ home/zsh-plugins/fzf.zsh
@@ -0,0 +1,18 @@
+export FZF_COMPLETION_TRIGGER='..'
+export FZF_COMPLETION_OPTS='+c -x'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS="--sort 20000"
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}

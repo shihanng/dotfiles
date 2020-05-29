bd0bd88eb66bbba5c2761dd81c95bad76b313181
Shi Han NG <shihanng@gmail.com>
2020-05-30 08:01:53 +0900

    nix: Add color in select_cdr

--- nix/zsh_plugins/select_cdr.zsh
+++ nix/zsh_plugins/select_cdr.zsh
@@ -1,7 +1,7 @@
 # Ref: https://petitviolet.hatenablog.com/entry/20190708/1562544000
 function select_cdr(){
     local selected_dir=$(cdr -l | awk '{ print $2 }' | \
-      fzf --preview 'f() { sh -c "ls -hFGl $1" }; f {}')
+      fzf --preview 'f() { sh -c "ls --color -hFGl $1" }; f {}')
     if [ -n "$selected_dir" ]; then
         BUFFER="cd ${selected_dir}"
         zle accept-line

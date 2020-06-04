d93835d0fb9bc8822738771b1e4ad46137068e77
Shi Han NG <shihanng@gmail.com>
2020-06-05 06:11:54 +0900

    nix: Fix lorri on arch

--- nix/arch-shihanng.nix
+++ nix/arch-shihanng.nix
@@ -5,5 +5,8 @@
   home.homeDirectory = "/home/shihanng";
   services.lorri.enable = true;
 
+  # See https://github.com/rycee/home-manager/issues/1265
+  systemd.user.services.lorri.Service.Environment = [ "NIX_PATH=${builtins.getEnv "NIX_PATH"}" ];
+
   imports = [ ./home.nix ];
 }

d82785f036cb090dacc05b788f34bbd49cd1382e
Shi Han NG <shihanng@gmail.com>
2020-07-14 22:47:29 +0900

    Remove zsh-plugins
    * Close #23
    * Close #22

--- home/zsh-plugins/anyframe.zsh
+++
@@ -1,5 +0,0 @@
-zstyle ":anyframe:selector:" use fzf
-zstyle ":anyframe:selector:fzf:" command 'fzf --extended --tiebreak=index'
-bindkey '^r' anyframe-widget-put-history
-bindkey '^o' anyframe-widget-cdr
-bindkey '^b' anyframe-widget-checkout-git-branch

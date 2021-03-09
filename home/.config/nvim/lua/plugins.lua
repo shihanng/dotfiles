7a73619f4938153278d7653951f3ee3ee0dc6932
Shi Han NG <shihanng@gmail.com>
2021-03-09 19:38:10 +0900

    nvim: Setup gopls

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -93,6 +93,57 @@ nvim_lsp.tsserver.setup {
     on_attach = on_attach
 }
 
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim
+nvim_lsp.gopls.setup {
+    cmd = {"gopls", "serve"},
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true
+            },
+            staticcheck = true
+        }
+    }
+}
+
+function GOIMPORTS(timeoutms)
+    local context = {source = {organizeImports = true}}
+    vim.validate {context = {context, "t", true}}
+
+    local params = vim.lsp.util.make_range_params()
+    params.context = context
+
+    -- See the implementation of the textDocument/codeAction callback
+    -- (lua/vim/lsp/handler.lua) for how to do this properly.
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeoutms)
+    if not result or next(result) == nil then
+        return
+    end
+    local actions = result[1].result
+    if not actions then
+        return
+    end
+    local action = actions[1]
+
+    -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+    -- is a CodeAction, it can have either an edit, a command or both. Edits
+    -- should be executed first.
+    if action.edit or type(action.command) == "table" then
+        if action.edit then
+            vim.lsp.util.apply_workspace_edit(action.edit)
+        end
+        if type(action.command) == "table" then
+            vim.lsp.buf.execute_command(action.command)
+        end
+    else
+        vim.lsp.buf.execute_command(action)
+    end
+end
+
+vim.api.nvim_exec([[
+autocmd BufWritePre *.go lua GOIMPORTS(1000)
+]], false)
+
 local system_name
 if vim.fn.has("mac") == 1 then
     system_name = "macOS"
---
+++ lsp/go.mod
@@ -0,0 +1,5 @@
+module github.com/shihanng/dotfiles/lsp
+
+go 1.15
+
+require golang.org/x/tools/gopls v0.6.6 // indirect

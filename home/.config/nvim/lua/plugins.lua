b61a8c489cc0fefc7f84c0cc592a0282df371073
Shi Han NG <shihanng@gmail.com>
2021-03-17 10:11:22 +0900

    nvim: Fix go fmt

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -202,45 +202,25 @@ nvim_lsp.gopls.setup {
             staticcheck = true
         }
     },
-    on_attach = function(client)
-        -- Because we are using _G.goimports
-        client.resolved_capabilities.document_formatting = false
-        on_attach(client)
-    end
+    on_attach = on_attach
 }
 
 _G.goimports = function(timeoutms)
     local context = {source = {organizeImports = true}}
     vim.validate {context = {context, "t", true}}
-
     local params = vim.lsp.util.make_range_params()
     params.context = context
 
-    -- See the implementation of the textDocument/codeAction callback
-    -- (lua/vim/lsp/handler.lua) for how to do this properly.
     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeoutms)
-    if not result or next(result) == nil then
+    if not result then
         return
     end
-    local actions = result[1].result
-    if not actions then
+    result = result[1].result
+    if not result then
         return
     end
-    local action = actions[1]
-
-    -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-    -- is a CodeAction, it can have either an edit, a command or both. Edits
-    -- should be executed first.
-    if action.edit or type(action.command) == "table" then
-        if action.edit then
-            vim.lsp.util.apply_workspace_edit(action.edit)
-        end
-        if type(action.command) == "table" then
-            vim.lsp.buf.execute_command(action.command)
-        end
-    else
-        vim.lsp.buf.execute_command(action)
-    end
+    local edit = result[1].edit
+    vim.lsp.util.apply_workspace_edit(edit)
 end
 
 vim.api.nvim_exec([[

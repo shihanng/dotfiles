aa6d7e6cea9dfdec7591cc0ce31cac71eb0080ba
Shi Han NG <shihanng@gmail.com>
2021-04-29 15:40:46 +0900

    lsp: Install gotests

--- lsp/tools.go
+++ lsp/tools.go
@@ -3,7 +3,7 @@
 package main
 
 import (
-	_ "github.com/cweill/gotests"
+	_ "github.com/cweill/gotests/gotests"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/lighttiger2505/sqls"
 	_ "github.com/mattn/efm-langserver"

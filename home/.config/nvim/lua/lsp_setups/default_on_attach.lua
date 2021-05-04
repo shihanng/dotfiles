c1669e78c79b487d0f5c00da96271fe89b321b44
Shi Han NG <shihanng@gmail.com>
2021-05-04 20:29:16 +0900

    nvim: Format default_on_attach.lua

--- home/.config/nvim/lua/lsp_setups/default_on_attach.lua
+++ home/.config/nvim/lua/lsp_setups/default_on_attach.lua
@@ -25,18 +25,21 @@ augroup END
         buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
     end
 
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
-      hi link LspReferenceRead Visual
-      hi link LspReferenceText Visual
-      hi link LspReferenceWrite Visual
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]], false)
-  end
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec(
+            [[
+hi link LspReferenceRead Visual
+hi link LspReferenceText Visual
+hi link LspReferenceWrite Visual
+augroup lsp_document_highlight
+  autocmd! * <buffer>
+  autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+  autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+augroup END
+            ]],
+            false
+        )
+    end
 end
 
 return default_on_attach

092abdde69c1e0f5cf505077a7e090beed6e1624
Shi Han NG <shihanng@gmail.com>
2021-05-05 12:37:46 +0900

    nvim: Add spell

---
+++ home/.config/nvim/lua/spell.lua
@@ -0,0 +1,6 @@
+vim.o.spelllang = "en,cjk"
+vim.bo.spelllang = "en,cjk"
+
+-- Toggle spell checking on and off.
+vim.api.nvim_set_keymap("n", "<Leader>s", [[:set spell!<CR>]], {silent = true})
+vim.api.nvim_set_keymap("n", "z=", ":lua require('telescope.builtin').spell_suggest()<cr>", {noremap = true})

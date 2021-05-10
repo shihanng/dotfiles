3920ea6f81568055fa018c82bd979b9bec63c487
Shi Han NG <shihanng@gmail.com>
2021-05-11 08:33:44 +0900

    nvim: Add LSP references keymap

--- home/.config/nvim/lua/lsp_setups/default_on_attach.lua
+++ home/.config/nvim/lua/lsp_setups/default_on_attach.lua
@@ -7,6 +7,7 @@ local function default_on_attach(client, bufnr)
 
     buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
     buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
     buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
     buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
     buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)

6f0d00c1a3d5be2f3ab8b185e51897983d4f238f
Shi Han NG <shihanng@gmail.com>
2021-10-16 19:05:56 +0900

    Add .wezterm.lua

---
+++ dot_wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require "wezterm"
+
+return {
+    color_scheme = "nord",
+    use_ime = true,
+    font = wezterm.font("FiraCode Nerd Font Mono"),
+    hide_tab_bar_if_only_one_tab = true
+}

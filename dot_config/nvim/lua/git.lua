d8c42ae6b1345bb3a8053b0a81a57554493a45db
Shi Han NG <shihanng@gmail.com>
2021-11-14 21:38:35 +0900

    nvim: Setup browsing for GitHub Enterprise

--- dot_config/nvim/lua/git.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_set_keymap("n", "<leader>gs", [[:G<CR>]], {})
-vim.api.nvim_set_keymap("n", "<leader>g]", [[:diffget //3<CR>]], {})
-vim.api.nvim_set_keymap("n", "<leader>g[", [[:diffget //2<CR>]], {})

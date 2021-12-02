0dd41ad4ab0e74e6338c96d51e103c860e29b64b
Shi Han NG <shihanng@gmail.com>
2021-12-03 07:40:40 +0900

    nvim: Comment out format_async

--- dot_config/nvim/lua/lsp_setups/default_on_attach.lua
+++ dot_config/nvim/lua/lsp_setups/default_on_attach.lua
@@ -1,20 +1,21 @@
-local format_async = function(err, result, ctx)
-    if err ~= nil or result == nil then
-        return
-    end
-
-    local bufnr = ctx.bufnr
-    if not vim.api.nvim_buf_get_option(bufnr, "modified") then
-        local view = vim.fn.winsaveview()
-        vim.lsp.util.apply_text_edits(result, bufnr)
-        vim.fn.winrestview(view)
-        if bufnr == vim.api.nvim_get_current_buf() then
-            vim.api.nvim_command("noautocmd :update")
-        end
-    end
-end
-
-vim.lsp.handlers["textDocument/formatting"] = format_async
+-- See: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/390#issuecomment-984850679
+-- local format_async = function(err, result, ctx)
+--     if err ~= nil or result == nil then
+--         return
+--     end
+--
+--     local bufnr = ctx.bufnr
+--     if not vim.api.nvim_buf_get_option(bufnr, "modified") then
+--         local view = vim.fn.winsaveview()
+--         vim.lsp.util.apply_text_edits(result, bufnr)
+--         vim.fn.winrestview(view)
+--         if bufnr == vim.api.nvim_get_current_buf() then
+--             vim.api.nvim_command("noautocmd :update")
+--         end
+--     end
+-- end
+--
+-- vim.lsp.handlers["textDocument/formatting"] = format_async
 
 local function default_on_attach(client, bufnr)
     local function buf_set_keymap(...)

163cd6090f280ccbd4357c18ab02476402f9db39
Shi Han NG <shihanng@gmail.com>
2021-12-03 07:37:21 +0900

    nvim: Prevent range formatting in gopls

--- dot_config/nvim/lua/lsp_setups/gopls.lua
+++ dot_config/nvim/lua/lsp_setups/gopls.lua
@@ -12,6 +12,7 @@ local M = {
     },
     on_attach = function(client, bufnr)
         client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
         on_attach(client, bufnr)
     end
 }

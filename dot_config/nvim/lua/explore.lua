81c82d655fa6ad64d482ba355e1bc7e9dba61fba
Shi Han NG <shihanng@gmail.com>
2022-01-03 11:30:52 +0900

    nvim: Fix Trouble breaking changes
    Fix #52

--- dot_config/nvim/lua/explore.lua
+++ dot_config/nvim/lua/explore.lua
@@ -49,22 +49,22 @@ vim.api.nvim_set_keymap("", "g*", [[<Plug>(asterisk-gz*)<Plug>(is-nohl-1)]], {})
 vim.api.nvim_set_keymap("", "#", [[<Plug>(asterisk-z#)<Plug>(is-nohl-1)]], {})
 vim.api.nvim_set_keymap("", "g#", [[<Plug>(asterisk-gz#)<Plug>(is-nohl-1)]], {})
 
-vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>LspTroubleToggle<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>TroubleToggle<cr>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap(
     "n",
     "<leader>xw",
-    "<cmd>LspTroubleToggle lsp_workspace_diagnostics<cr>",
+    "<cmd>TroubleToggle workspace_diagnostics<cr>",
     {silent = true, noremap = true}
 )
 vim.api.nvim_set_keymap(
     "n",
     "<leader>xd",
-    "<cmd>LspTroubleToggle lsp_document_diagnostics<cr>",
+    "<cmd>TroubleToggle document_diagnostics<cr>",
     {silent = true, noremap = true}
 )
-vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>LspTroubleToggle loclist<cr>", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>LspTroubleToggle quickfix<cr>", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("n", "gR", "<cmd>LspTrouble lsp_references<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", {silent = true, noremap = true})
 
 vim.api.nvim_set_keymap("n", "<F5>", [[:UndotreeToggle<CR>]], {noremap = true})
 

a4d0578bc8b16b86fabbcb6076bc3ed29ba2ae09
Shi Han NG <shihanng@gmail.com>
2022-01-22 12:11:53 +0900

    nvim: Add luacheck in nullls

--- dot_config/nvim/lua/lsp_setups/nullls.lua
+++ dot_config/nvim/lua/lsp_setups/nullls.lua
@@ -5,15 +5,16 @@ local on_attach = require("lsp_setups/default_on_attach")
 local M = {
     on_attach = on_attach,
     sources = {
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.diagnostics.golangci_lint,
         null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.diagnostics.golangci_lint,
+        null_ls.builtins.diagnostics.luacheck,
         null_ls.builtins.formatting.clang_format.with(
             {
                 filetypes = {"c", "cpp", "cs", "java", "proto"}
             }
-        )
+        ),
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.prettierd
     }
 }
 

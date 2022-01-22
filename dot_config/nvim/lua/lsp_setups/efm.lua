bf1fb771d772e2d4281caab3a8709dc162f55506
Shi Han NG <shihanng@gmail.com>
2022-01-22 12:31:14 +0900

    nvim: Replace luafmt with stylua

--- dot_config/nvim/lua/lsp_setups/efm.lua
+++ dot_config/nvim/lua/lsp_setups/efm.lua
@@ -1,22 +1,21 @@
 local on_attach = require("lsp_setups/default_on_attach")
 
 local M = {
-    filetypes = {
-        "elixir",
-        "eruby",
-        "lua",
-        "ruby",
-        "sh",
-        "sql"
-    },
-    init_options = {
-        documentFormatting = true,
-        hover = true,
-        documentSymbol = true,
-        codeAction = true,
-        completion = true
-    },
-    on_attach = on_attach
+	filetypes = {
+		"elixir",
+		"eruby",
+		"ruby",
+		"sh",
+		"sql",
+	},
+	init_options = {
+		documentFormatting = true,
+		hover = true,
+		documentSymbol = true,
+		codeAction = true,
+		completion = true,
+	},
+	on_attach = on_attach,
 }
 
 return M
--- dot_config/nvim/lua/lsp_setups/nullls.lua
+++ dot_config/nvim/lua/lsp_setups/nullls.lua
@@ -3,19 +3,20 @@ local null_ls = require("null-ls")
 local on_attach = require("lsp_setups/default_on_attach")
 
 local M = {
-    on_attach = on_attach,
-    sources = {
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.diagnostics.luacheck,
-        null_ls.builtins.formatting.clang_format.with(
-            {
-                filetypes = {"c", "cpp", "cs", "java", "proto"}
-            }
-        ),
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.prettierd
-    }
-}
+	on_attach = on_attach,
+	sources = {
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.diagnostics.golangci_lint,
+		null_ls.builtins.diagnostics.luacheck,
+		null_ls.builtins.formatting.clang_format.with({
+			filetypes = { "c", "cpp", "cs", "java", "proto" },
+		}),
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.formatting.prettierd,
 
+		-- https://github.com/JohnnyMorganz/StyLua
+		-- Manually install the binary from release into ~/bin
+		null_ls.builtins.formatting.stylua,
+	},
+}
 return M

46c4a0409156b43b292a9d22d45868d77fd25efc
Shi Han NG <shihanng@gmail.com>
2022-01-23 22:20:54 +0900

    nvim: Add checkmake

--- dot_config/nvim/lua/lsp_setups/nullls.lua
+++ dot_config/nvim/lua/lsp_setups/nullls.lua
@@ -1,10 +1,37 @@
 local null_ls = require("null-ls")
+local helpers = require("null-ls.helpers")
 
 local on_attach = require("lsp_setups/default_on_attach")
 
+local checkmake = {
+	method = null_ls.methods.DIAGNOSTICS,
+	filetypes = { "make" },
+	generator = null_ls.generator({
+		command = "checkmake",
+		args = {
+			[[--format]],
+			[[{{.LineNumber}}|{{.Rule}}: {{.Violation}}{{"\n"}}]],
+			"$FILENAME",
+		},
+		to_stdin = false,
+		from_stderr = true,
+		format = "raw",
+		to_temp_file = true,
+		on_output = helpers.diagnostics.from_errorformat(
+			table.concat({
+				[[%l|%m]],
+				[[%-G\\s%#]],
+			}, ","),
+			"checkmake"
+		),
+	}),
+}
+
 local M = {
+	debug = true,
 	on_attach = on_attach,
 	sources = {
+		checkmake,
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.diagnostics.golangci_lint,
 		null_ls.builtins.diagnostics.luacheck,

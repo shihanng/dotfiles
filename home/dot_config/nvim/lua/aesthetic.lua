71d804f3725ed57a196403aa1e5178e633061ef0
Shi Han NG <shihanng@gmail.com>
2022-03-13 12:06:29 +0900

    nvim: Install textsubjects

--- home/dot_config/nvim/lua/aesthetic.lua
+++ home/dot_config/nvim/lua/aesthetic.lua
@@ -19,6 +19,15 @@ require("nvim-treesitter.configs").setup({
 	autotag = {
 		enable = true,
 	},
+	textsubjects = {
+		enable = true,
+		prev_selection = ",", -- (Optional) keymap to select the previous selection
+		keymaps = {
+			["."] = "textsubjects-smart",
+			[";"] = "textsubjects-container-outer",
+			["i;"] = "textsubjects-container-inner",
+		},
+	},
 })
 
 vim.g.nvcode_termcolors = 256
--- home/dot_config/nvim/lua/packer_plugins.lua
+++ home/dot_config/nvim/lua/packer_plugins.lua
@@ -20,7 +20,6 @@ return require("packer").startup(function(use)
 	use({ "onsails/lspkind-nvim" })
 	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
 	use({ "tversteeg/registers.nvim" })
-	use({ "windwp/nvim-ts-autotag" })
 
 	-- Coding
 	use({ "neovim/nvim-lspconfig" })
@@ -36,7 +35,8 @@ return require("packer").startup(function(use)
 		requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 	})
 	use({ "jose-elias-alvarez/nvim-lsp-ts-utils" })
-
+	use({ "RRethy/nvim-treesitter-textsubjects" })
+	use({ "windwp/nvim-ts-autotag" })
 	use({ "b3nj5m1n/kommentary" })
 	use({ "cohama/lexima.vim" })
 	use({ "JoosepAlviste/nvim-ts-context-commentstring" })

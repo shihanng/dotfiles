5a01b3b18d71b32ca1f98253a57c089104ebeed8
Shi Han NG <shihanng@gmail.com>
2022-05-18 10:17:42 +0900

    nvim: Add basic configs for dap

--- home/dot_config/nvim/lua/dap_debug.lua
+++ home/dot_config/nvim/lua/dap_debug.lua
@@ -1,2 +1,43 @@
-require("dap-go").setup()
 require("dapui").setup()
+
+local dap = require("dap")
+local dap_go = require("dap-go")
+local dap_ui = require("dapui")
+
+dap_go.setup()
+dap_ui.setup()
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dap_ui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dap_ui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dap_ui.close()
+end
+
+vim.api.nvim_set_keymap("n", "<F7>", ":lua require'dap'.continue()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F8>", ":lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F9>", ":lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F10>", ":lua require'dap'.step_out()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>b",
+	":lua require'dap'.toggle_breakpoint()<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>B",
+	":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>lp",
+	":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap("n", "<Leader>dr", ":lua require'dap'.repl.open()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>dl", ":lua require'dap'.run_last()<CR>", { noremap = true, silent = true })

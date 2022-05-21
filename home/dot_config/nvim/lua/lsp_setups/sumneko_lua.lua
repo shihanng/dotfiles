a3a215a873acff735202eca75980b5bcb546a13c
Shi Han NG <shihanng@gmail.com>
2022-05-21 21:16:34 +0900

    nvim: Simplify lua-language-server setup

--- home/dot_config/nvim/lua/lsp_setups/sumneko_lua.lua
+++ home/dot_config/nvim/lua/lsp_setups/sumneko_lua.lua
@@ -1,36 +1,18 @@
 local on_attach = require("lsp_setups/default_on_attach")
 
-local system_name
-if vim.fn.has("mac") == 1 then
-	system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-	system_name = "Linux"
-elseif vim.fn.has("win32") == 1 then
-	system_name = "Windows"
-else
-	print("Unsupported system for sumneko")
-end
-
-local sumneko_root_path = vim.fn.expand("~/dev/github.com/sumneko/lua-language-server")
-local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
-
 local M = {
-	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
 	on_attach = on_attach,
 	settings = {
 		Lua = {
 			runtime = {
 				version = "LuaJIT",
-				path = vim.split(package.path, ";"),
 			},
 			diagnostics = {
 				globals = { "vim" },
 			},
 			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-				},
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
 			},
 			telemetry = {
 				enable = false,

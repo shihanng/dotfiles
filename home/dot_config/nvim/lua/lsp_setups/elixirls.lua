4438357543953a9738997bdb64dfd692561b704f
Shi Han NG <shihanng@gmail.com>
2022-08-07 11:57:22 +0900

    nvim: Fix elixir-ls path

--- home/dot_config/nvim/lua/lsp_setups/elixirls.lua
+++ home/dot_config/nvim/lua/lsp_setups/elixirls.lua
@@ -1,6 +1,6 @@
 local on_attach = require("lsp_setups/default_on_attach")
 
-local ls_path = vim.fn.expand("~/dev/github.com/elixir-lsp/elixir-ls/rel/language_server.sh")
+local ls_path = vim.fn.expand("~/dev/me/github.com/elixir-lsp/elixir-ls/rel/language_server.sh")
 
 local M = {
 	cmd = { ls_path },

32b7993270a6b95b8cfe6e079e7ce77edabc3cfc
Shi Han NG <shihanng@gmail.com>
2022-12-11 14:58:03 +0900

    neovim: Show vim-visual-multi status in lualine

--- home/dot_config/nvim/lua/aesthetic.lua
+++ home/dot_config/nvim/lua/aesthetic.lua
@@ -50,6 +50,17 @@ if vim.fn.has("termguicolors") == 1 then
 	vim.opt.termguicolors = true
 end
 
+-- Shows vim-visual-multi status in lualine
+local vim_visual_multi = function()
+	local vm_status_ok, VMInfos = pcall(vim.fn.VMInfos)
+	-- Check if VMInfos exists, check if the return of VMInfos is {}
+	if (not vm_status_ok) or (not next(VMInfos)) then
+		return ""
+	end
+
+	return "V-M " .. VMInfos["status"] .. " " .. VMInfos["ratio"]
+end
+
 -- This make sure that the statusline for dap-ui is not changing when the window is active.
 -- https://github.com/rcarriga/nvim-dap-ui/issues/50#issuecomment-905804897
 local dap_line = {
@@ -63,6 +74,9 @@ require("lualine").setup({
 		component_separators = { left = "", right = "" },
 	},
 	extensions = { dap_line, "fugitive", "quickfix", "nvim-tree" },
+	sections = {
+		lualine_a = { "mode", vim_visual_multi },
+	},
 })
 
 require("gitsigns").setup()
--- home/dot_config/nvim/lua/multi.lua
+++ home/dot_config/nvim/lua/multi.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_set_var("VM_default_mappings", 1)
+vim.api.nvim_set_var("VM_set_statusline", 3)
 
 vim.api.nvim_set_var("VM_maps", {
 	["Find Under"] = "mm",

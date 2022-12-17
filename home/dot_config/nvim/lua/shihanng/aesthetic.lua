976a77dec9c53f1fce93e7fa89f28e7a501615dc
Shi Han NG <shihanng@gmail.com>
2022-12-17 12:27:46 +0900

    neovim: Refactor treesitter

--- home/dot_config/nvim/lua/shihanng/aesthetic.lua
+++ home/dot_config/nvim/lua/shihanng/aesthetic.lua
@@ -1,35 +1,3 @@
-require("nvim-treesitter.configs").setup({
-	ensure_installed = "all",
-	ignore_install = { "haskell" },
-	indent = {
-		enable = true,
-	},
-	highlight = {
-		enable = true,
-	},
-	rainbow = {
-		enable = true,
-		extended_mode = true,
-		max_file_lines = nil,
-	},
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
-	autotag = {
-		enable = true,
-	},
-	textsubjects = {
-		enable = true,
-		prev_selection = ",", -- (Optional) keymap to select the previous selection
-		keymaps = {
-			["."] = "textsubjects-smart",
-			[";"] = "textsubjects-container-outer",
-			["i;"] = "textsubjects-container-inner",
-		},
-	},
-})
-
 vim.g.nvcode_termcolors = 256
 
 vim.opt.syntax = "on"

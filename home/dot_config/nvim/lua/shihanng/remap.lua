7f7d3c0795136bc0d696bee228277266c89c2582
Shi Han NG <shihanng@gmail.com>
2022-12-17 23:44:54 +0900

    neovim: Add some cool stuffs

---
+++ home/dot_config/nvim/lua/shihanng/remap.lua
@@ -0,0 +1,10 @@
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])

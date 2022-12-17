1fe4bf624660dce304c98fdbdebda6dc433f1d2d
Shi Han NG <shihanng@gmail.com>
2022-12-17 12:27:46 +0900

    neovim: Refactor treesitter

---
+++ home/dot_config/nvim/after/plugin/treesitter.lua
@@ -0,0 +1,31 @@
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "all",
+	indent = {
+		enable = true,
+	},
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+		max_file_lines = nil,
+	},
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	autotag = {
+		enable = true,
+	},
+	textsubjects = {
+		enable = true,
+		prev_selection = ",", -- (Optional) keymap to select the previous selection
+		keymaps = {
+			["."] = "textsubjects-smart",
+			[";"] = "textsubjects-container-outer",
+			["i;"] = "textsubjects-container-inner",
+		},
+	},
+})

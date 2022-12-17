70107f3c944116db2d2389e990c1a9ba23113246
Shi Han NG <shihanng@gmail.com>
2022-12-17 22:46:00 +0900

    neovim: Add lspkind to cmp

--- home/dot_config/nvim/after/plugin/lsp.lua
+++ home/dot_config/nvim/after/plugin/lsp.lua
@@ -88,6 +88,36 @@ lsp.on_attach(function(client, bufnr)
 	end
 end)
 
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<CR>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
+
+-- disable completion with tab
+-- this helps with copilot setup
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings,
+	formatting = {
+		format = require("lspkind").cmp_format({
+			mode = "symbol_text",
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			before = function(entry, vim_item)
+				return vim_item
+			end,
+		}),
+	},
+})
+
 lsp.nvim_workspace()
 
 lsp.setup()

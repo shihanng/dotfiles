1ab6e7cd1e06d9b4699b785de92c5ede983993ff
Shi Han NG <shihanng@gmail.com>
2022-12-17 21:34:14 +0900

    neovim: Migrate ufo configs

--- home/dot_config/nvim/after/plugin/lsp.lua
+++ home/dot_config/nvim/after/plugin/lsp.lua
@@ -1,14 +1,24 @@
+-- https://github.com/kevinhwang91/nvim-ufo
+-- Use :lua =vim.lsp.get_active_clients()[1] to check if foldingRange
+-- is properly set.
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+capabilities.textDocument.foldingRange = {
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
+}
+
 local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 lsp.ensure_installed({
 	"gopls",
 	"ltex",
+	"sumneko_lua",
 })
 
-lsp.nvim_workspace()
-
 lsp.configure("gopls", {
+	capabilities = capabilities,
 	settings = {
 		gopls = {
 			analyses = {
@@ -17,6 +27,17 @@ lsp.configure("gopls", {
 			staticcheck = true,
 		},
 	},
+})
+
+-- share options between serveral servers
+local lsp_opts = {
+	capabilities = capabilities,
+}
+
+lsp.setup_servers({
+	"ltex",
+	"sumneko_lua",
+	opts = lsp_opts,
 })
 
 -- Setup to only use one LSP for formatting.
@@ -67,19 +88,13 @@ lsp.on_attach(function(client, bufnr)
 	end
 end)
 
+lsp.nvim_workspace()
+
 lsp.setup()
 
 local null_ls = require("null-ls")
 local null_ls_helpers = require("null-ls.helpers")
 local null_opts = lsp.build_options("null-ls", {})
-
--- Copy from default_setup.
--- What about other LSPs?
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-capabilities.textDocument.foldingRange = {
-	dynamicRegistration = false,
-	lineFoldingOnly = true,
-}
 
 local checkmake = {
 	method = null_ls.methods.DIAGNOSTICS,
@@ -149,3 +164,76 @@ null_ls.setup({
 		null_ls.builtins.formatting.stylua,
 	},
 })
+
+local ufo = require("ufo")
+local ftMap = {
+	-- vim = "indent",
+	-- python = { "indent" },
+	-- git = "",
+}
+
+local handler = function(virtText, lnum, endLnum, width, truncate)
+	local newVirtText = {}
+	local suffix = ("  %d "):format(endLnum - lnum)
+	local sufWidth = vim.fn.strdisplaywidth(suffix)
+	local targetWidth = width - sufWidth
+	local curWidth = 0
+	for _, chunk in ipairs(virtText) do
+		local chunkText = chunk[1]
+		local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+		if targetWidth > curWidth + chunkWidth then
+			table.insert(newVirtText, chunk)
+		else
+			chunkText = truncate(chunkText, targetWidth - curWidth)
+			local hlGroup = chunk[2]
+			table.insert(newVirtText, { chunkText, hlGroup })
+			chunkWidth = vim.fn.strdisplaywidth(chunkText)
+			-- str width returned from truncate() may less than 2nd argument, need padding
+			if curWidth + chunkWidth < targetWidth then
+				suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+			end
+			break
+		end
+		curWidth = curWidth + chunkWidth
+	end
+	table.insert(newVirtText, { suffix, "MoreMsg" })
+	return newVirtText
+end
+
+ufo.setup({
+	open_fold_hl_timeout = 150,
+	close_fold_kinds = { "imports", "comment" },
+	preview = {
+		win_config = {
+			border = { "", "─", "", "", "", "─", "", "" },
+			winhighlight = "Normal:Folded",
+			winblend = 0,
+		},
+		mappings = {
+			scrollU = "<C-u>",
+			scrollD = "<C-d>",
+		},
+	},
+	fold_virt_text_handler = handler,
+	provider_selector = function(_, filetype, _)
+		-- return a string type use internal providers
+		-- return a string in a table like a string type
+		-- return empty string '' will disable any providers
+		-- return `nil` will use default value {'lsp', 'indent'}
+
+		-- if you prefer treesitter provider rather than lsp,
+		-- return ftMap[filetype] or {'treesitter', 'indent'}
+		return ftMap[filetype]
+	end,
+})
+
+vim.keymap.set("n", "zR", ufo.openAllFolds)
+vim.keymap.set("n", "zM", ufo.closeAllFolds)
+vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds)
+vim.keymap.set("n", "zm", ufo.closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+vim.keymap.set("n", "K", function()
+	local winid = ufo.peekFoldedLinesUnderCursor()
+	if not winid then
+		vim.lsp.buf.hover()
+	end
+end)

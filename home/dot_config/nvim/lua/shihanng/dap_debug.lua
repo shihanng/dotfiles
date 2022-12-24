658c858e2a22319ec17ee3dd58f7e64caab1b663
Shi Han NG <shihanng@gmail.com>
2022-12-24 22:37:53 +0900

    neovim: Refactor dap_debug

--- home/dot_config/nvim/lua/shihanng/dap_debug.lua
+++ home/dot_config/nvim/lua/shihanng/dap_debug.lua
@@ -1,67 +1,33 @@
 require("nvim-dap-virtual-text").setup()
 require("telescope").load_extension("dap")
-
 require("mason-nvim-dap").setup({
 	ensure_installed = { "delve", "python" },
 	automatic_setup = false,
 })
-
 require("dap-python").setup(vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python")
+require("dap-go").setup()
 
-local dap_go = require("dap-go")
+local dap = require("dap")
 local dap_ui = require("dapui")
 
-dap_go.setup()
 dap_ui.setup()
 
-vim.api.nvim_set_keymap("n", "<Leader>dc", ":Telescope dap configurations<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>dn", ":lua require'dap'.continue()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>dv", ":lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>di", ":lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>do", ":lua require'dap'.step_out()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>dk", ":lua require'dap'.up()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>dj", ":lua require'dap'.down()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>dt", ":lua require'dap'.terminate()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<M-k>", ":lua require'dapui'.eval()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>df", ":Telescope dap frames<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<Leader>db", ":Telescope dap list_breakpoints<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>d?",
-	":lua require'dapui'.float_element('scopes', {enter=true})<CR>",
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>ds",
-	":lua require'dapui'.float_element('stacks', {enter=true})<CR>",
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>b",
-	":lua require'dap'.toggle_breakpoint()<CR>",
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>B",
-	":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>lp",
-	":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap(
-	"n",
-	"<Leader>de",
-	":lua require'dapui'.float_element('repl', {enter=true})<CR>",
-	{ noremap = true, silent = true }
-)
-vim.api.nvim_set_keymap("n", "<Leader>dl", ":lua require'dap'.run_last()<CR>", { noremap = true, silent = true })
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "<F1>", require("dap").step_over, opts)
+vim.keymap.set("n", "<F2>", require("dap").step_into, opts)
+vim.keymap.set("n", "<F3>", require("dap").step_out, opts)
+vim.keymap.set("n", "<F5>", require("dap").continue, opts)
+vim.keymap.set("n", "<F6>", require("dap").run_to_cursor, opts)
+vim.keymap.set("n", "<F9>", require("dap").up, opts)
+vim.keymap.set("n", "<F10>", require("dap").down, opts)
+vim.keymap.set("n", "<Leader>dt", require("dap").terminate, opts)
+vim.keymap.set("n", "<Leader>db", require("telescope").extensions.dap.list_breakpoints, opts)
+vim.keymap.set("n", "<Leader>b", require("dap").toggle_breakpoint, opts)
+vim.keymap.set("n", "<Leader>B", function()
+	dap.set_breakpoint(vim.fn.input("Cond: "))
+end, opts)
+vim.keymap.set("n", "<Leader>dd", dap_ui.toggle)
 
 -- Customizing color scheme in dap-ui
 -- https://github.com/rcarriga/nvim-dap-ui/blob/e5c32746aa72e39267803fdf6934d79541d39f42/lua/dapui/config/highlights.lua#L1
@@ -92,3 +58,13 @@ vim.fn.sign_define(
 )
 
 vim.cmd("au FileType dap-repl lua require('dap.ext.autocompl').attach()")
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dap_ui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dap_ui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dap_ui.close()
+end

4755cfae3b61e17e7ce74c1c892e7fc321102524
Shi Han NG <shihanng@gmail.com>
2022-12-28 19:32:39 +0900

    neovim: Use gopls to fix import

--- home/dot_config/nvim/after/plugin/lsp.lua
+++ home/dot_config/nvim/after/plugin/lsp.lua
@@ -104,7 +104,6 @@ local lsp_formatting = function(options)
 	opts.filter = function(client)
 		-- Get client name from :LspInfo
 		local no_format_list = {
-			gopls = true,
 			jsonls = true,
 			pyright = true,
 			sumneko_lua = true,
@@ -146,6 +145,26 @@ lsp.on_attach(function(client, bufnr)
 		vim.keymap.set("v", "<space>f", lsp_formatting, opts)
 	end
 end)
+
+-- Auto goimports with gopls
+-- https://github.com/neovim/nvim-lspconfig/issues/115
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.go" },
+	callback = function()
+		local params = vim.lsp.util.make_range_params()
+		local wait_ms = 500
+		params.context = { only = { "source.organizeImports" } }
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+		for cid, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+					vim.lsp.util.apply_workspace_edit(r.edit, enc)
+				end
+			end
+		end
+	end,
+})
 
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }

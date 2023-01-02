2e37816c1d78324800d6ef2c070ec3f1a5e87916
Shi Han NG <shihanng@gmail.com>
2023-01-02 13:30:42 +0900

    nix: First commit
    Here we go again.

---
+++ nix/home.nix
@@ -0,0 +1,8 @@
+{pkgs, ...}: {
+    home.username = "shihanng";
+    home.homeDirectory = "/home/shihanng";
+    home.stateVersion = "23.05";
+    programs.home-manager.enable = true;
+    home.packages = [
+    ];
+}

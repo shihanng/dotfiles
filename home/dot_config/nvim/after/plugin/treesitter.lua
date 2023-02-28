382b559f6f99fb967926582b768abdbeca4fa845
Shi Han NG <shihanng@gmail.com>
2023-02-28 21:18:18 +0900

    nvim: Disable treesitter highlight for gitcommit
    I can't get it highlight git commit message that is too long.

--- home/dot_config/nvim/after/plugin/treesitter.lua
+++ home/dot_config/nvim/after/plugin/treesitter.lua
@@ -15,6 +15,7 @@ require("nvim-treesitter.configs").setup({
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
+		disable = { "gitcommit" },
 	},
 	rainbow = {
 		enable = true,

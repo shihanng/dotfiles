47be7cd194a6864d5b3a8df4a93e627fb9087025
Shi Han NG <shihanng@gmail.com>
2023-07-04 10:23:34 +0900

    Change harpoon add binding

--- home/dot_config/nvim/after/plugin/harpoon.lua
+++ home/dot_config/nvim/after/plugin/harpoon.lua
@@ -1,5 +1,5 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>ha", mark.add_file)
 vim.keymap.set("n", "<leader>he", ui.toggle_quick_menu)

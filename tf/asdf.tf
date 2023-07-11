19673a14d39e0880d1c7554a9f714f18db08f484
Shi Han NG <shihanng@gmail.com>
2023-07-11 14:11:13 +0900

    Use devbox to install terraform and goreleaser

--- tf/asdf.tf
+++ tf/asdf.tf
@@ -20,10 +20,6 @@ module "installer" {
       git_url  = "https://github.com/skyzyx/asdf-tflint"
       versions = ["0.44.1"]
     }
-    terraform = {
-      git_url  = "https://github.com/asdf-community/asdf-hashicorp.git"
-      versions = ["1.3.5"]
-    }
     terraform-ls = {
       git_url  = "https://github.com/asdf-community/asdf-hashicorp.git"
       versions = ["0.30.1"]

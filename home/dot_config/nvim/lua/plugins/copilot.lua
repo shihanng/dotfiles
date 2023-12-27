c83262d855e86e878ff722a5ccdca292348d8b81
Shi Han NG <shihanng@gmail.com>
2023-12-27 13:47:11 +0800

    nvim: Disable Copilot by default

--- home/dot_config/nvim/lua/plugins/copilot.lua
+++ home/dot_config/nvim/lua/plugins/copilot.lua
@@ -3,5 +3,12 @@ return {
 	config = function()
 		vim.keymap.set("i", "<C-j>", "<Plug>(copilot-next)", { noremap = false, silent = true })
 		vim.keymap.set("i", "<C-k>", "<Plug>(copilot-previous)", { noremap = false, silent = true })
+
+		vim.g.copilot_filetypes = {
+			["*"] = false, -- Disable Copilot globally
+			-- We disable Copilot globally,
+			-- Use the following to enable for specific language:
+			-- ["javascript"] = true, -- Enable for specific filetypes, e.g., JavaScript
+		}
 	end,
 }

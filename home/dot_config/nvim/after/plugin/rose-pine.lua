397a309238179efd03598bed26abae11040af86a
Shi Han NG <shihanng@gmail.com>
2024-05-31 08:22:15 +0900

    nvim: Replace nvim_get_hl_by_name

--- home/dot_config/nvim/after/plugin/rose-pine.lua
+++ home/dot_config/nvim/after/plugin/rose-pine.lua
@@ -9,8 +9,7 @@ vim.cmd.colorscheme("rose-pine")
 
 -- Use color that does have same background color
 -- in the LSP diagnostics (the window that shows up during <space>e).
-local hi_normal = vim.api.nvim_get_hl_by_name("Normal", true)
-vim.api.nvim_set_hl(0, "NormalFloat", hi_normal)
+vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
 
 -- Set color of LspInlayHint to be the same as Comment.
 vim.api.nvim_set_hl(0, "LspInlayHint", { link = "Comment" })

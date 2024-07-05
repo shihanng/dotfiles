18286def0a1112c143000ba93799d594e703feb8
Shi Han NG <shihanng@gmail.com>
2024-07-05 12:16:06 +0900

    Use ctrl hjkl to navigate between nvim and zellij

--- home/dot_config/nvim/lua/plugins/init.lua
+++ home/dot_config/nvim/lua/plugins/init.lua
@@ -29,20 +29,14 @@ return {
     "direnv/direnv.vim",
     "mattn/emmet-vim",
     {
-        "https://git.sr.ht/~swaits/zellij-nav.nvim",
-        lazy = true,
-        event = "VeryLazy",
-        keys = {
-            { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
-            { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
-            { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
-            { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
-        },
-        opts = {},
-    },
-    {
         "mrcjkb/rustaceanvim",
         version = "^4", -- Recommended
         ft = { "rust" },
+    },
+    {
+        "hiasr/vim-zellij-navigator.nvim",
+        config = function()
+            require("vim-zellij-navigator").setup()
+        end,
     },
 }
--- home/dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ home/dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,17 +1,12 @@
 return {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-        "TmuxNavigateLeft",
-        "TmuxNavigateDown",
-        "TmuxNavigateUp",
-        "TmuxNavigateRight",
-        "TmuxNavigatePrevious",
-    },
+    "https://git.sr.ht/~swaits/zellij-nav.nvim",
+    lazy = true,
+    event = "VeryLazy",
     keys = {
-        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
+        { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+        { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+        { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
     },
+    opts = {},
 }

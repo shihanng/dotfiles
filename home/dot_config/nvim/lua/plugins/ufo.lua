dc353b8e0fe3abdf692297c1a25ae701d171e874
Shi Han NG <shihanng@gmail.com>
2024-08-12 13:26:37 +0900

    nvim: Fix sign for ufo

--- home/dot_config/nvim/lua/plugins/ufo.lua
+++ home/dot_config/nvim/lua/plugins/ufo.lua
@@ -20,7 +20,7 @@ return {
         -- See: https://github.com/kevinhwang91/nvim-ufo?tab=readme-ov-file#customize-fold-text
         local handler = function(virtText, lnum, endLnum, width, truncate)
             local newVirtText = {}
-            local suffix = ("  %d "):format(endLnum - lnum)
+            local suffix = ("  %d "):format(endLnum - lnum)
             local sufWidth = vim.fn.strdisplaywidth(suffix)
             local targetWidth = width - sufWidth
             local curWidth = 0

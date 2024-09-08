1610a1f5f12a0408b5c953259066e24845940767
Shi Han NG <shihanng@gmail.com>
2024-09-09 08:19:38 +0900

    nvim: Update zellij-nav

--- home/dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ home/dot_config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -3,18 +3,10 @@ return {
     lazy = true,
     event = "VeryLazy",
     keys = {
-        {
-            "<c-h>",
-            function() require("zellij-nav").left("move-focus-or-tab") end,
-            { silent = true, desc = "navigate left" },
-        },
+        { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>", { silent = true, desc = "navigate left" } },
         { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
         { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
-        {
-            "<c-l>",
-            function() require("zellij-nav").right("move-focus-or-tab") end,
-            { silent = true, desc = "navigate right" },
-        },
+        { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { silent = true, desc = "navigate right" } },
     },
     opts = {},
 }

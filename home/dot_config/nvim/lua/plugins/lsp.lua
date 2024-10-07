27f9c57256223d50b7e7a388a0e702e57292aefe
Shi Han NG <shihanng@gmail.com>
2024-10-07 21:52:39 +0900

    nvim: Fix rustaceanvim codelldb path

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -99,14 +99,34 @@ return {
 
             -- Setup rustaceanvim with lsp-zero.
             -- See: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/quick-recipes.md
-            vim.g.rustaceanvim = {
-                server = {
-                    capabilities = lsp_zero.get_capabilities(),
-                },
-                dap = {
-                    autoload_configurations = true,
-                },
-            }
+            vim.g.rustaceanvim = function()
+                -- Update this path
+                local extension_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/"
+                local codelldb_path = extension_path .. "adapter/codelldb"
+                local liblldb_path = extension_path .. "lldb/lib/liblldb"
+                local this_os = vim.uv.os_uname().sysname
+
+                -- The path is different on Windows
+                if this_os:find("Windows") then
+                    codelldb_path = extension_path .. "adapter\\codelldb.exe"
+                    liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+                else
+                    -- The liblldb extension is .so for Linux and .dylib for MacOS
+                    liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+                end
+
+                local cfg = require("rustaceanvim.config")
+                return {
+
+                    server = {
+                        capabilities = lsp_zero.get_capabilities(),
+                    },
+                    dap = {
+                        -- autoload_configurations = true,
+                        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+                    },
+                }
+            end
 
             -- Auto goimports with gopls
             -- https://github.com/neovim/nvim-lspconfig/issues/115#issuecomment-1128115341

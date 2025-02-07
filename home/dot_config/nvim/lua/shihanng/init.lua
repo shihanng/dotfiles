7fd0b55f51de6757d7efb6e28281e339fad0bc40
Shi Han NG <shihanng@gmail.com>
2025-02-07 15:20:19 +0900

    nvim: Remove git_wp

--- home/dot_config/nvim/lua/shihanng/init.lua
+++ home/dot_config/nvim/lua/shihanng/init.lua
@@ -96,5 +96,3 @@ vim.api.nvim_set_keymap("n", "<leader>g[", [[:diffget //2<CR>]], {})
 vim.api.nvim_set_keymap("n", "<leader>gd", [[:Gvdiff!<CR>]], {})
 
 vim.cmd([[ command! -nargs=1 Browse silent exec '!open "<args>"' ]])
-
-require("shihanng.git_wp")

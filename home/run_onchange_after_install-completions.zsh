ebed7f703045621a67bc67ed6acd7c237e8ce001
Shi Han NG <shihanng@gmail.com>
2025-02-09 20:39:15 +0900

    zim: Use shihanng/zim-zellij

--- home/run_onchange_after_install-completions.zsh
+++ home/run_onchange_after_install-completions.zsh
@@ -2,5 +2,4 @@
 atuin gen-completions --shell zsh --out-dir $HOME/.completions/
 kind completion zsh > $HOME/.completions/_kind
 kustomize completion zsh > $HOME/.completions/_kustomize
-zellij setup --generate-completion zsh > $HOME/.completions/_zellij
 granted completion -s zsh > $HOME/.completions/_granted

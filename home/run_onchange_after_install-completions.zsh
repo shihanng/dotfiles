5904c190e2904473d9314c01401f0a60f80aeeb7
Shi Han NG <shihanng@gmail.com>
2025-02-10 10:49:01 +0900

    zim: Use shihanng/zim-granted

--- home/run_onchange_after_install-completions.zsh
+++ home/run_onchange_after_install-completions.zsh
@@ -2,4 +2,3 @@
 atuin gen-completions --shell zsh --out-dir $HOME/.completions/
 kind completion zsh > $HOME/.completions/_kind
 kustomize completion zsh > $HOME/.completions/_kustomize
-granted completion -s zsh > $HOME/.completions/_granted

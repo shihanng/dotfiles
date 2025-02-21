63c87485372fa1b4a342d55fc6912cfed1889776
Shi Han NG <shihanng@gmail.com>
2025-02-21 14:32:51 +0900

    nvim: Do not set background for LspInlayHint

--- home/dot_config/nvim/after/plugin/catppuccin.lua
+++ home/dot_config/nvim/after/plugin/catppuccin.lua
@@ -7,3 +7,6 @@ require("catppuccin").setup({
 })
 
 vim.cmd.colorscheme("catppuccin")
+local current = vim.api.nvim_get_hl_by_name("LspInlayHint", true)
+current.bg = "none"
+vim.api.nvim_set_hl(0, "LspInlayHint", current)

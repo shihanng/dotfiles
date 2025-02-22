6ca1e4d6ba8643324906c8c8f5f2e64bd4ce66b2
Shi Han NG <shihanng@gmail.com>
2025-02-22 18:04:21 +0900

    nvim: Use mini.files

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -9,4 +9,18 @@ return {
         version = "*",
         config = function() require("mini.surround").setup() end,
     },
+    {
+        "echasnovski/mini.icons",
+        version = "*",
+        config = function() require("mini.icons").setup() end,
+    },
+    {
+        "echasnovski/mini.files",
+        version = "*",
+        config = function()
+            require("mini.files").setup()
+
+            vim.keymap.set("n", "-", require("mini.files").open, { desc = "Open parent ectory" })
+        end,
+    },
 }

8190d11a800d1cef8dc775ab0c52706134824d77
Shi Han NG <shihanng@gmail.com>
2025-02-23 15:21:35 +0900

    nvim: Simplify Trouble setup

--- home/dot_config/nvim/lua/plugins/trouble.lua
+++ home/dot_config/nvim/lua/plugins/trouble.lua
@@ -1,6 +1,6 @@
 return {
     "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Trouble",
     keys = {
         {
             "<leader>xx",
@@ -16,21 +16,6 @@ return {
             "<leader>cs",
             "<cmd>Trouble symbols toggle focus=false<cr>",
             desc = "Symbols (Trouble)",
-        },
-        {
-            "<leader>cl",
-            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-            desc = "LSP Definitions / references / ... (Trouble)",
-        },
-        {
-            "<leader>xL",
-            "<cmd>Trouble loclist toggle<cr>",
-            desc = "Location List (Trouble)",
-        },
-        {
-            "<leader>xQ",
-            "<cmd>Trouble qflist toggle<cr>",
-            desc = "Quickfix List (Trouble)",
         },
     },
     opts = {},

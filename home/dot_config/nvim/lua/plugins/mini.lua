5af8c1315f5985c940e4fa2c443585fee47c2b60
Shi Han NG <shihanng@gmail.com>
2025-02-23 09:13:17 +0900

    nvim: Use mini.comment

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -9,6 +9,15 @@ return {
             require("mini.files").setup()
             require("mini.align").setup()
             require("mini.pairs").setup()
+            require("mini.comment").setup({
+                options = {
+                    custom_commentstring = function()
+                        -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#minicomment
+                        -- See comment.lua
+                        return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+                    end,
+                },
+            })
 
             vim.keymap.set("n", "-", require("mini.files").open, { desc = "Open parent ectory" })
         end,

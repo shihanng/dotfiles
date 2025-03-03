64b5259c2c973186c2e60f27b8c01722208b8852
Shi Han NG <shihanng@gmail.com>
2025-03-03 17:32:46 +0900

    nvim: Fix snacks type

--- home/dot_config/nvim/lua/plugins/snacks.lua
+++ home/dot_config/nvim/lua/plugins/snacks.lua
@@ -3,6 +3,8 @@ return {
         "folke/snacks.nvim",
         priority = 1000,
         lazy = false,
+
+        ---@module "snacks"
         ---@type snacks.Config
         opts = {
             bigfile = { enabled = true },

2892611fd0f35c0fc64f67e999404f5dec150ab6
Shi Han NG <shihanng@gmail.com>
2025-03-22 13:17:42 +0900

    Remove mini.jump2d from Neovim

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -19,7 +19,6 @@ return {
                 },
             })
             require("mini.jump").setup()
-            require("mini.jump2d").setup()
 
             vim.keymap.set(
                 "n",

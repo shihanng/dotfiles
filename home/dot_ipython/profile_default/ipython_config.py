036e535ffb92b5e0899a34438a27f83ef8ff16f9
Shi Han NG <shihanng@gmail.com>
2025-04-06 17:59:04 +0900

    Fix unreadable exception in IPython
    Prior to this change, IPython renders the exception in an unreadable
    color.
    
    This change fixes the issue following this article:
    https://heemayl.net/posts/emacs-change-ipython-traceback-background-color-from-yellow-to-black/

--- home/dot_ipython/profile_default/ipython_config.py
+++ home/dot_ipython/profile_default/ipython_config.py
@@ -1,5 +1,14 @@
 # Configuration file for ipython.
 
+try:
+    from IPython.core import ultratb
+
+    ultratb.VerboseTB.tb_highlight = "bg:ansiblack"
+except Exception:
+    print(
+        "Error patching background color for tracebacks, they'll be the ugly default instead"
+    )
+
 c = get_config()  # noqa
 
 # ------------------------------------------------------------------------------

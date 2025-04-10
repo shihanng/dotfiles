ac412ae57eb574470bec6e54254bdb528def5fc8
Shi Han NG <shihanng@gmail.com>
2025-04-10 21:50:55 +0900

    Setup catppuccin theme for fzf

--- home/dot_zsh/fzf.zsh
+++ home/dot_zsh/fzf.zsh
@@ -2,7 +2,13 @@ export FZF_COMPLETION_TRIGGER='..'
 export FZF_COMPLETION_OPTS='+c -x'
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_DEFAULT_OPTS="--sort 20000"
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
+--color=selected-bg:#494d64 \
+--color=border:#363a4f,label:#cad3f5 \
+--sort 20000"
 
 # Use fd (https://github.com/sharkdp/fd) instead of the default find
 # command for listing path candidates.

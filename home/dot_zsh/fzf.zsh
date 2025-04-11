a09dbf34f6e1352bd8e8d193cf47f58fa7a06df0
Shi Han NG <shihanng@gmail.com>
2025-04-11 11:34:01 +0900

    Open in Neovim from rfv

--- home/dot_zsh/fzf.zsh
+++ home/dot_zsh/fzf.zsh
@@ -40,5 +40,5 @@ rfv() {
       --header 'CTRL-T: Switch between ripgrep/fzf' \
       --preview 'bat --color=always {1} --highlight-line {2}' \
       --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
-      --bind 'enter:become(vim {1} +{2})'
+      --bind 'enter:become(nvim {1} +{2})'
 }

48eb495331a6a47c09a440f40b69d077b8094464
Shi Han NG <shihanng@gmail.com>
2025-05-05 18:37:21 +0900

    Use ,l to clear reset CopilotChat window
    Prior to this change, the default keymap was <C-l>
    which conflicts with the keymap for moving the focus of window
    to the left.

--- home/dot_config/nvim/lua/plugins/copilot.lua
+++ home/dot_config/nvim/lua/plugins/copilot.lua
@@ -24,7 +24,12 @@ return {
             },
             build = "make tiktoken", -- Only on MacOS or Linux
             opts = {
-                -- See Configuration section for options
+                mappings = {
+                    reset = {
+                        normal = "<Leader>l",
+                        insert = "",
+                    },
+                },
             },
             -- See Commands section for default commands if you want to lazy load on them
         },

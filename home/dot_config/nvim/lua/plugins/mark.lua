d94078f0e0b001e93fc3ecfd2f557ce0248a8ad8
Shi Han NG <shihanng@gmail.com>
2025-05-06 10:10:20 +0900

    Use ,ml to trigger recall picker

--- home/dot_config/nvim/lua/plugins/mark.lua
+++ home/dot_config/nvim/lua/plugins/mark.lua
@@ -21,6 +21,6 @@ return {
         vim.keymap.set("n", "<leader>mn", recall.goto_next, { noremap = true, silent = true })
         vim.keymap.set("n", "<leader>mp", recall.goto_prev, { noremap = true, silent = true })
         vim.keymap.set("n", "<leader>mc", recall.clear, { noremap = true, silent = true })
-        vim.keymap.set("n", "<C-m>", require("recall.snacks").pick, { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>ml", require("recall.snacks").pick, { noremap = true, silent = true })
     end,
 }

7136650a85fb7dec426c9b1ee853a8acc7214928
Shi Han NG <shihanng@gmail.com>
2025-06-02 06:39:24 +0900

    Enable taplo LSP for TOML files

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -154,6 +154,7 @@ return {
                 "postgres_lsp",
                 "pyrefly",
                 "ruff",
+                "taplo",
                 "terraformls",
                 "tflint",
                 "yamlls",

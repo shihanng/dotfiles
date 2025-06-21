221e9522490860a20f02edd8dc3fe792d5513322
Shi Han NG <shihanng@gmail.com>
2025-06-21 15:06:33 +0900

    Update multicursor plugin keybindings

--- home/dot_config/nvim/lua/plugins/visual-multi.lua
+++ home/dot_config/nvim/lua/plugins/visual-multi.lua
@@ -9,16 +9,16 @@ return {
         local set = vim.keymap.set
 
         -- Add or skip cursor above/below the main cursor.
-        set({ "n", "v" }, "<up>", function() mc.lineAddCursor(-1) end)
-        set({ "n", "v" }, "<down>", function() mc.lineAddCursor(1) end)
-        set({ "n", "v" }, "<leader><up>", function() mc.lineSkipCursor(-1) end)
-        set({ "n", "v" }, "<leader><down>", function() mc.lineSkipCursor(1) end)
+        set({ "n", "x" }, "<up>", function() mc.lineAddCursor(-1) end)
+        set({ "n", "x" }, "<down>", function() mc.lineAddCursor(1) end)
+        set({ "n", "x" }, "<leader><up>", function() mc.lineSkipCursor(-1) end)
+        set({ "n", "x" }, "<leader><down>", function() mc.lineSkipCursor(1) end)
 
         -- Add or skip adding a new cursor by matching word/selection
-        set({ "n", "v" }, "<leader>n", function() mc.matchAddCursor(1) end)
-        set({ "n", "v" }, "<leader>s", function() mc.matchSkipCursor(1) end)
-        set({ "n", "v" }, "<leader>N", function() mc.matchAddCursor(-1) end)
-        set({ "n", "v" }, "<leader>S", function() mc.matchSkipCursor(-1) end)
+        set({ "n", "x" }, "<leader>n", function() mc.matchAddCursor(1) end)
+        set({ "n", "x" }, "<leader>s", function() mc.matchSkipCursor(1) end)
+        set({ "n", "x" }, "<leader>N", function() mc.matchAddCursor(-1) end)
+        set({ "n", "x" }, "<leader>S", function() mc.matchSkipCursor(-1) end)
 
         -- Add all matches in the document
         set({ "n", "v" }, "<leader>A", mc.matchAllAddCursors)

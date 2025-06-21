c4bef8cca498b9bd553d08d4bf485d7dd77391c5
Shi Han NG <shihanng@gmail.com>
2025-06-21 15:03:28 +0900

    Rebind set spell to ,SP
    Prior to this change, ,s is conflicting with multiple cursors plugin.

--- home/dot_config/nvim/lua/shihanng/init.lua
+++ home/dot_config/nvim/lua/shihanng/init.lua
@@ -88,7 +88,7 @@ vim.api.nvim_set_keymap("", "<Leader>jq", [[:%!jq '.'<cr>]], { noremap = true })
 
 -- Spellcheck
 vim.opt.spelllang = { "en", "cjk" }
-vim.api.nvim_set_keymap("n", "<Leader>s", [[:set spell!<CR>]], { silent = true }) -- Toggle spell checking on and off.
+vim.api.nvim_set_keymap("n", "<Leader>SP", [[:set spell!<CR>]], { silent = true }) -- Toggle spell checking on and off.
 
 vim.cmd([[ command! -nargs=1 Browse silent exec '!open "<args>"' ]])
 

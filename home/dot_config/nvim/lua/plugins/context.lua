62d45f409aad6edf7e4e4e092c59e9eb0636c9fd
Shi Han NG <shihanng@gmail.com>
2025-07-21 11:58:25 +0900

    Split long line in Lua

--- home/dot_config/nvim/lua/plugins/context.lua
+++ home/dot_config/nvim/lua/plugins/context.lua
@@ -7,7 +7,10 @@ return {
             min_window_height = 1, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
             line_numbers = true,
             multiline_threshold = 20, -- Maximum number of lines to show for a single context
-            trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+
+            -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+            trim_scope = "outer",
+
             mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
             -- Separator between context and content. Should be a single character string, like '-'.
             -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.

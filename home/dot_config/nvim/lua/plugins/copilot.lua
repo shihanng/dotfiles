adcfcc8eda8f06418610cff7ee7c8462851aa1b4
Shi Han NG <shihanng@gmail.com>
2025-08-10 10:53:06 +0900

    Cleanup copilot-lsp setup

--- home/dot_config/nvim/lua/plugins/copilot.lua
+++ home/dot_config/nvim/lua/plugins/copilot.lua
@@ -17,16 +17,31 @@ return {
     {
         "copilotlsp-nvim/copilot-lsp",
         init = function()
+            require("copilot-lsp").setup({
+                nes = {
+                    move_count_threshold = 2, -- Clear after 2 movements
+                },
+            })
+
             vim.g.copilot_nes_debounce = 500
+
             vim.keymap.set("n", "<tab>", function()
-                -- Try to jump to the start of the suggestion edit.
-                -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
-                local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
-                    or (
-                        require("copilot-lsp.nes").apply_pending_nes()
-                        and require("copilot-lsp.nes").walk_cursor_end_edit()
-                    )
-            end)
+                local bufnr = vim.api.nvim_get_current_buf()
+                local state = vim.b[bufnr].nes_state
+                if state then
+                    -- Try to jump to the start of the suggestion edit.
+                    -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+                    local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+                        or (
+                            require("copilot-lsp.nes").apply_pending_nes()
+                            and require("copilot-lsp.nes").walk_cursor_end_edit()
+                        )
+                    return nil
+                else
+                    -- Resolving the terminal's inability to distinguish between `TAB` and `<C-i>` in normal mode
+                    return "<C-i>"
+                end
+            end, { desc = "Accept Copilot NES suggestion", expr = true })
         end,
     },
     {

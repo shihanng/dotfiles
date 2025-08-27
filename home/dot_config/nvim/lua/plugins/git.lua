10a3a1214b735b1cf74909f36872c07917517104
Shi Han NG <shihanng@gmail.com>
2025-08-27 19:50:11 +0900

    Setup JJ

--- home/dot_config/nvim/lua/plugins/git.lua
+++ home/dot_config/nvim/lua/plugins/git.lua
@@ -8,4 +8,12 @@ return {
             vim.keymap.set("n", "<leader>gx", [[:DiffviewClose <CR>]], { noremap = true, silent = true })
         end,
     },
+    {
+        "julienvincent/hunk.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        cmd = { "DiffEditor" },
+        config = function() require("hunk").setup() end,
+    },
+    { "avm99963/vim-jjdescription" },
+    { "rafikdraoui/jj-diffconflicts" },
 }

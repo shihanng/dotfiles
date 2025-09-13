7f20ab4d883ff77ee3368eb988043e9274cb8348
Shi Han NG <shihanng@gmail.com>
2025-09-13 21:20:21 +0900

    Migrate to nvim-mini/mini.nvim

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
     {
-        "echasnovski/mini.nvim",
+        "nvim-mini/mini.nvim",
         version = "*",
         config = function()
             local gen_spec = require("mini.ai").gen_spec
@@ -67,6 +67,27 @@ return {
                     map_split(buf_id, "<C-s>", "belowright horizontal")
                     map_split(buf_id, "<C-v>", "belowright vertical")
                     map_split(buf_id, "<C-t>", "tab")
+                end,
+            })
+
+            local show_dotfiles = true
+
+            local filter_show = function(_fs_entry) return true end
+
+            local filter_hide = function(fs_entry) return not vim.startswith(fs_entry.name, ".") end
+
+            local toggle_dotfiles = function()
+                show_dotfiles = not show_dotfiles
+                local new_filter = show_dotfiles and filter_show or filter_hide
+                MiniFiles.refresh({ content = { filter = new_filter } })
+            end
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesBufferCreate",
+                callback = function(args)
+                    local buf_id = args.data.buf_id
+                    -- Tweak left-hand side of mapping to your liking
+                    vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
                 end,
             })
         end,

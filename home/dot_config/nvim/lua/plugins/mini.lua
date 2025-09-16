98d4d872a3be49fbed321ea8573125aea52e2cf5
Shi Han NG <shihanng@gmail.com>
2025-09-16 16:07:14 +0900

    Open mini.files in buffer path

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -32,8 +32,10 @@ return {
             require("mini.files").setup()
         end,
         init = function()
-            local minifiles_toggle = function(...)
-                if not MiniFiles.close() then MiniFiles.open(...) end
+            local minifiles_toggle = function()
+                local buf_name = vim.api.nvim_buf_get_name(0)
+                local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
+                local _ = MiniFiles.close() or MiniFiles.open(path)
             end
 
             vim.keymap.set("n", "<C-n>", minifiles_toggle, { silent = true })

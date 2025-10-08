05e3ff465524d00538101cbf8580f91c2f155c16
Shi Han NG <shihanng@gmail.com>
2025-10-08 10:36:48 +0900

    Use sidekick.nvim

--- home/dot_config/nvim/lua/plugins/cmp.lua
+++ home/dot_config/nvim/lua/plugins/cmp.lua
@@ -27,21 +27,13 @@ return {
             keymap = {
                 preset = "enter",
                 ["<Tab>"] = {
-                    function(cmp)
-                        if vim.b[vim.api.nvim_get_current_buf()].nes_state then
-                            cmp.hide()
-                            return (
-                                require("copilot-lsp.nes").apply_pending_nes()
-                                and require("copilot-lsp.nes").walk_cursor_end_edit()
-                            )
-                        end
-                        if cmp.snippet_active() then
-                            return cmp.accept()
-                        else
-                            return cmp.select_and_accept()
-                        end
-                    end,
                     "snippet_forward",
+                    function() -- sidekick next edit suggestion
+                        return require("sidekick").nes_jump_or_apply()
+                    end,
+                    -- function() -- if you are using Neovim's native inline completions
+                    --   return vim.lsp.inline_completion.get()
+                    -- end,
                     "fallback",
                 },
                 ["<C-b>"] = { "show" },

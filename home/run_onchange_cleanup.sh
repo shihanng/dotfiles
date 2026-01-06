f3322b1534e27c9227b5000bf5dea798d14723ed
Shi Han NG <shihanng@gmail.com>
2026-01-06 11:41:05 +0800

    Use jjgi

--- home/run_onchange_cleanup.sh
+++ home/run_onchange_cleanup.sh
@@ -5,3 +5,4 @@ set -euxo pipefail
 rm -rf "${HOME}/.config/mcphub"
 rm -f "${HOME}/.config/nvim/lua/shihanng/fidget-spinner.lua"
 rm -f "${HOME}/.config/opencode/agent/commit.md"
+rm -rf "${HOME}/.scripts"

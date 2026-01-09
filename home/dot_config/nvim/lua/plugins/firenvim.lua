6e97cec31be5bdee3bd1272b4ae917c5badbeb80
Shi Han NG <shihanng@gmail.com>
2026-01-09 10:42:59 +0800

    Replace firenvim with GhostText

--- home/dot_config/nvim/lua/plugins/firenvim.lua
+++ home/dot_config/nvim/lua/plugins/firenvim.lua
@@ -1,26 +1,3 @@
 return {
-    "glacambre/firenvim",
-    build = ":call firenvim#install(0)",
-    init = function()
-        vim.g.firenvim_config = {
-            globalSettings = { alt = "all" },
-            localSettings = {
-                [".*"] = {
-                    selector = "textarea",
-                    takeover = "never",
-                },
-            },
-        }
-    end,
-    config = function()
-        vim.api.nvim_create_autocmd({ "UIEnter" }, {
-            callback = function(_event)
-                local client = vim.api.nvim_get_chan_info(vim.v.event.chan).client
-                if client ~= nil and client.name == "Firenvim" then
-                    vim.opt.guifont = "IosevkaTerm Nerd Font:h20"
-                    vim.o.laststatus = 0
-                end
-            end,
-        })
-    end,
+    { "subnut/nvim-ghost.nvim" },
 }

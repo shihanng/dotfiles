da86c629f78b15f7f0a64b873281eef2b2228507
Shi Han NG <shihanng@gmail.com>
2026-03-08 20:04:24 +0900

    Uninstall diffview.nvim

--- home/dot_config/nvim/lua/plugins/git.lua
+++ home/dot_config/nvim/lua/plugins/git.lua
@@ -1,14 +1,5 @@
 return {
     {
-        "sindrets/diffview.nvim",
-        config = function()
-            require("diffview").setup({})
-
-            vim.keymap.set("n", "<leader>gd", [[:DiffviewOpen <CR>]], { noremap = true, silent = true })
-            vim.keymap.set("n", "<leader>gx", [[:DiffviewClose <CR>]], { noremap = true, silent = true })
-        end,
-    },
-    {
         "esmuellert/codediff.nvim",
         cmd = "CodeDiff",
     },

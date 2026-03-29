fc01ce5e7dea8372509e8641643eb10d54c64b94
Shi Han NG <shihanng@gmail.com>
2026-03-29 16:02:30 +0900

    Replace text-case with coerce.nvim

--- home/dot_config/nvim/lua/plugins/text-case.lua
+++ home/dot_config/nvim/lua/plugins/text-case.lua
@@ -1,15 +1,5 @@
--- https://github.com/johmsalas/text-case.nvim?tab=readme-ov-file#example-for-lazyvim
 return {
-    "johmsalas/text-case.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    -- Author's Note: If default keymappings fail to register (possible config issue in my local setup),
-    -- verify lazy loading functionality. On failure, disable lazy load and report issue
-    -- lazy = false,
-    config = function()
-        require("textcase").setup({})
-        require("telescope").load_extension("textcase")
-    end,
-    keys = {
-        { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "v" }, desc = "Telescope" },
-    },
+    "gregorias/coerce.nvim",
+    tag = "v4.1.0",
+    config = true,
 }

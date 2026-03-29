d666da8d514307e15cdda8b52e17d4a18bbfaf83
Shi Han NG <shihanng@gmail.com>
2026-03-29 16:27:00 +0900

    Install vim-matchup
    Close #89

--- home/dot_config/nvim/lua/plugins/search.lua
+++ home/dot_config/nvim/lua/plugins/search.lua
@@ -25,4 +25,19 @@ return {
             })
         end,
     },
+    {
+        "andymass/vim-matchup",
+        init = function()
+            -- modify your configuration vars here
+            vim.g.matchup_treesitter_stopline = 500
+
+            -- or call the setup function provided as a helper. It defines the
+            -- configuration vars for you
+            require("match-up").setup({
+                treesitter = {
+                    stopline = 500,
+                },
+            })
+        end,
+    },
 }

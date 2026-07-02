4c16164f94ea98f82d08148ab6df313c8af7ca86
Shi Han NG <shihanng@gmail.com>
2026-07-02 20:34:12 +0900

    Migrate fully to sidekick

--- home/dot_config/nvim/lua/plugins/copilot.lua
+++ home/dot_config/nvim/lua/plugins/copilot.lua
@@ -24,128 +24,43 @@ return {
                     backend = "zellij",
                     enabled = true,
                 },
+                prompts = {
+                    commit = "@commit-author, read and improve commit message in {file}.",
+                    proofreader = "@content-proofreader, proofread {this}.",
+                    pair = "Pair with me in neovim. Connect to " .. require("shihanng.mcp_socket"),
+                },
             },
         },
         keys = {
             {
-                "<tab>",
-                function()
-                    -- if there is a next edit, jump to it, otherwise apply it if any
-                    if not require("sidekick").nes_jump_or_apply() then
-                        return "<Tab>" -- fallback to normal tab
-                    end
-                end,
-                expr = true,
-                desc = "Goto/Apply Next Edit Suggestion",
-            },
-            {
-                "<leader>aa",
-                function() require("sidekick.cli").toggle() end,
-                mode = { "n", "v" },
-                desc = "Sidekick Toggle CLI",
-            },
-            {
                 "<leader>as",
-                function() require("sidekick.cli").select() end,
-                -- Or to select only installed tools:
-                -- require("sidekick.cli").select({ filter = { installed = true } })
-                desc = "Sidekick Select CLI",
+                function() require("sidekick.cli").select({ filter = { installed = true } }) end,
+                desc = "Select CLI",
             },
             {
-                "<leader>as",
-                function() require("sidekick.cli").send({ selection = true }) end,
-                mode = { "v" },
-                desc = "Sidekick Send Visual Selection",
+                "<leader>at",
+                function() require("sidekick.cli").send({ msg = "{this}" }) end,
+                mode = { "x", "n" },
+                desc = "Send This",
+            },
+            {
+                "<leader>af",
+                function() require("sidekick.cli").send({ msg = "{file}" }) end,
+                desc = "Send File",
+            },
+            {
+                "<leader>av",
+                function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+                mode = { "x" },
+                desc = "Send Visual Selection",
             },
             {
                 "<leader>ap",
                 function() require("sidekick.cli").prompt() end,
-                mode = { "n", "v" },
+                mode = { "n", "x" },
                 desc = "Sidekick Select Prompt",
             },
-            {
-                "<c-.>",
-                function() require("sidekick.cli").focus() end,
-                mode = { "n", "x", "i", "t" },
-                desc = "Sidekick Switch Focus",
-            },
-            -- Example of a keybinding to open Claude directly
-            {
-                "<leader>ac",
-                function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
-                desc = "Sidekick Claude Toggle",
-                mode = { "n", "v" },
-            },
         },
-    },
-    {
-        "NickvanDyke/opencode.nvim",
-        dependencies = {
-            -- Recommended for better prompt input, and required to use
-            -- opencode.nvim's embedded terminal — otherwise optional
-            { "folke/snacks.nvim", opts = { input = {}, picker = {} } },
-        },
-        config = function()
-            vim.g.opencode_opts = {
-                -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on `opencode_opts`.
-                select = {
-                    prompts = {
-                        commit = "@commit-author, read and improve commit message in @buffer.",
-                        proofreader = "@content-proofreader, proofread @this.",
-                        pair = "Pair with me in neovim. Connect to " .. require("shihanng.mcp_socket"),
-                    },
-                },
-                lsp = {
-                    enabled = false,
-                },
-            }
-
-            -- Required for `vim.g.opencode_opts.auto_reload`.
-            vim.o.autoread = true
-
-            vim.keymap.set(
-                { "n", "x" },
-                "<leader>oa",
-                function() require("opencode").ask("@this: ", { submit = true }) end,
-                { desc = "Ask opencode…" }
-            )
-            vim.keymap.set(
-                { "n", "x" },
-                "<leader>os",
-                function() require("opencode").select() end,
-                { desc = "Execute opencode action…" }
-            )
-            vim.keymap.set(
-                { "n", "t" },
-                "<leader>ot",
-                function() require("opencode").toggle() end,
-                { desc = "Toggle opencode" }
-            )
-
-            vim.keymap.set(
-                { "n", "x" },
-                "go",
-                function() return require("opencode").operator("@this ") end,
-                { desc = "Add range to opencode", expr = true }
-            )
-            vim.keymap.set(
-                "n",
-                "goo",
-                function() return require("opencode").operator("@this ") .. "_" end,
-                { desc = "Add line to opencode", expr = true }
-            )
-
-            -- Listen for `opencode` events
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "OpencodeEvent",
-                callback = function(_args)
-                    -- See the available event types and their properties
-                    -- vim.notify(vim.inspect(args.data.event))
-                    -- Do something useful
-                    -- if args.data.event.type == "session.idle" then vim.notify("`opencode` finished responding") end
-                end,
-            })
-        end,
     },
     {
         "linw1995/nvim-mcp",
